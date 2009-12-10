16f18cb187d5d5086dd559822682744d8b01f6c4
Nicholas Henry <nicholas.henry@gmail.com>
2009-12-09 23:07:08 -0500

    Complete protoype spec

--- specs/lib/prototype_spec.js
+++ specs/lib/prototype_spec.js
@@ -11,5 +11,12 @@ Screw.Unit(function() {
       another_stooge["first-name"] = "Nicholas"
       expect(stooge["first-name"]).to(equal, "Jerome");
     });
+
+    it("should make new properties added to the prototype available to it's objects", function(){      
+      stooge["alias"] = "Jerome the Bone"
+      expect(another_stooge["alias"]).to(equal, "Jerome the Bone");
+    });
+
+
   });
 });

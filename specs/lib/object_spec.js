bef308036fe8699a022a6e4c449701e99e34f2f0
Nicholas Henry <nicholas.henry@gmail.com>
2010-02-23 21:23:43 -0500

    Refactored object tests and added enumeration description

--- specs/lib/object_spec.js
+++ specs/lib/object_spec.js
@@ -1,5 +1,5 @@
 Screw.Unit(function() {
-  describe('Object Literals', function() {
+  describe('Objects', function() {
     before(function(){
       stooge = {
         "first-name": "Jerome",
@@ -22,22 +22,29 @@ Screw.Unit(function() {
       }
 
     });
-    it("defined by curly braces", function(){
-      my_object = {};
-      expect(typeof my_object).to(equal, 'object');
-    });
     
-    it("has zero or more name/value pairs")
+    /* Literals 
+    */
+    describe('Literals', function() {
+      it("defined by curly braces", function(){
+        my_object = {};
+        expect(typeof my_object).to(equal, 'object');
+      });
+    
+      it("has zero or more name/value pairs")
 
-    it("has property names as a string", function() {
-      expect(stooge["first-name"]).to(equal, "Jerome");
-    });
+      it("has property names as a string", function() {
+        expect(stooge["first-name"]).to(equal, "Jerome");
+      });
     
-    it("can be nested", function() {
-      expect(flight.departure.IATA).to(equal, "SYD")
+      it("can be nested", function() {
+        expect(flight.departure.IATA).to(equal, "SYD")
+      });
     });
-    
-    describe('retrieval', function() {
+
+    /* Retrieval
+    */    
+    describe('Retrieval', function() {
       it("can retrieve values using the [] suffix", function() {
         expect(stooge["first-name"]).to(equal, "Jerome");
       });
@@ -61,8 +68,10 @@ Screw.Unit(function() {
       });
       
     });
-    
-    describe('updating', function(){
+
+    /* Update
+    */    
+    describe('Update', function(){
       it("can update by assignment", function(){
         expect(stooge['first-name']).to(equal, "Jerome");
         stooge['first-name'] = "Joe";
@@ -86,6 +95,78 @@ Screw.Unit(function() {
       }); 
       
     });
-    
+
+     /* Prototype
+     */
+     describe('Protoype', function() {
+       before(function(){
+         another_stooge = Object.beget(stooge);
+       });
+       it("should not change the prototype when the object is updated", function(){
+         another_stooge["first-name"] = "Nicholas"
+         expect(stooge["first-name"]).to(equal, "Jerome");
+       });
+
+       it("should make new properties added to the prototype available to it's objects", function(){      
+         stooge["alias"] = "Jerome the Bone"
+         expect(another_stooge["alias"]).to(equal, "Jerome the Bone");
+       });
+     });
+
+     /* Reflection
+     */
+     describe('Reflection', function() {
+       it("should determine the type of the property", function(){
+         expect(typeof flight.number).to(equal, "number");
+         expect(typeof flight.airline).to(equal, "string");
+         expect(typeof flight.arrival).to(equal, "object");
+         expect(typeof flight.manifest).to(equal, "undefined");
+       });
+
+       it("should produce a value for any property", function(){
+         expect(typeof flight.toString).to(equal, "function");
+         expect(typeof flight.constructor).to(equal, "function");
+       });    
+
+       it("should reject functions with hasOwnProperty ", function(){
+         expect(flight.hasOwnProperty('number')).to(equal, true);
+         expect(flight.hasOwnProperty('constructor')).to(equal, false);
+       });
+
+       it("should not look up the prototype chain with hasOwnProperty", function(){
+         another_flight = Object.beget(flight);
+         expect(another_flight.hasOwnProperty('number')).to(equal, false);
+       });        
+     });
+     
+     /* Enumeration
+     */
+     describe('Enumeration', function() {
+       it("can loop over property names in an object", function() {
+         name;
+         properties = [];
+         for (name in stooge) {
+           if (typeof another_stooge[name] !== 'function') {
+             properties.push(name);
+           }
+         }
+         expect(properties).to(equal, ["first-name", "last-name"]);
+       });
+       
+     });              
+     /* Delete
+     */
+     describe('Delete', function() {
+       before(function(){
+         stooge.nickname = "Curly";
+         another_stooge = Object.beget(stooge);
+         another_stooge.nickname = "Moe";
+       });       
+       it("should remove a property from an object", function(){
+         expect(another_stooge.nickname).to(equal, "Moe");
+         delete another_stooge.nickname;
+         expect(another_stooge.nickname).to(equal, "Curly");
+       });
+     });
   });
 });
